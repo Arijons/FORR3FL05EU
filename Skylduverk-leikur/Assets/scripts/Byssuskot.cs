804017ff4bd7587b9301eb5787d1f84cb8fd51ae
Ari Jónsson <arijons20@gmail.com>
2019-10-12 20:17:49 +0000

    Script fyrir byssu og fleira

---
+++ Skylduverk-leikur/Assets/scripts/Byssuskot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Byssuskot : MonoBehaviour
+{ public AudioSource gunsound;  
+   
+// Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            gunsound = GetComponent<AudioSource>();
+            gunsound.Play();
+            GetComponent<Animation>().Play("Byssuskot");
+        }
+    }
+}
