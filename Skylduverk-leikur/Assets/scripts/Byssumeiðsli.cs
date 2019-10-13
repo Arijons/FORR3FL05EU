2bd580d701759f45ed2f4f6d91946caceae08769
Ari Jónsson <arijons20@gmail.com>
2019-10-13 13:25:59 +0000

    Kláruð byssu og óvinsskrftin

---
+++ Skylduverk-leikur/Assets/scripts/Byssumeiðsli.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Byssumeiðsli : MonoBehaviour
+{
+    public int MagnMeiðsla = 5;
+    public float FjarlægðÓvinar;
+    public float BysssaDrifur = 15;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit Skot;
+
+            {
+                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Skot))
+                {
+                    FjarlægðÓvinar = Skot.distance;
+                    if (FjarlægðÓvinar < BysssaDrifur)
+                    {
+                        Skot.transform.SendMessage("MinnkaHeilsu", MagnMeiðsla);
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+           
+     
+
+
---
+++ Skylduverk-leikur/Assets/scripts/Óvinascript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Óvinascript : MonoBehaviour
+{
+    public int HeilsaÓvinar = 10;
+
+    
+
+
+    void MinnkaHeilsu(int MagnMeiðsla)
+    {
+        HeilsaÓvinar = -MagnMeiðsla;
+    }
+
+    private void Update()
+    {
+        if (HeilsaÓvinar<0)
+        {
+            Destroy(gameObject);
+        }
+    }
+ 
+ }
+
+
+
+
+
+
+
