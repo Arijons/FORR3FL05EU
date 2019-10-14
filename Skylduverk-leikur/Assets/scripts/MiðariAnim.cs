ea68de17ddf0e9eb766011554f956f1049c221dd
Ari Jónsson <arijons20@gmail.com>
2019-10-14 22:54:57 +0000

    Laga mið

--- Skylduverk-leikur/Assets/scripts/MiðariAnim.cs
+++ Skylduverk-leikur/Assets/scripts/MiðariAnim.cs
@@ -9,7 +9,19 @@ public class MiðariAnim : MonoBehaviour
     public GameObject LeftCurs;
     public GameObject RightCurs;
 
+    IEnumerator Bið()
+    {
+        yield return new WaitForSeconds(0.1F);
+    }
 
+    void WA()
+    {
+        UpCurs.GetComponent<Animator>().enabled = false;
+        DownCurs.GetComponent<Animator>().enabled = false;
+        LeftCurs.GetComponent<Animator>().enabled = false;
+        RightCurs.GetComponent<Animator>().enabled = false;
+
+    }
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
@@ -18,18 +30,13 @@ public class MiðariAnim : MonoBehaviour
             DownCurs.GetComponent<Animator>().enabled = true;
             LeftCurs.GetComponent<Animator>().enabled = true;
             RightCurs.GetComponent<Animator>().enabled = true;
-            WA();
 
+            Bið();
+            WA();
+       
         }
     }
-     void WA()
-    {   
-        UpCurs.GetComponent<Animator>().enabled = false;
-        DownCurs.GetComponent<Animator>().enabled = false;
-        LeftCurs.GetComponent<Animator>().enabled = false;
-        RightCurs.GetComponent<Animator>().enabled = false;
 
-    }
 }
 
 
