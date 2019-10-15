d81cc83d2b5a429bbcbe6841a5b0bb7c0736d80d
Ari Jónsson <arijons20@gmail.com>
2019-10-15 20:16:31 +0000

    Skotfæra geymslur og hljóð

--- Skylduverk-leikur/Assets/scripts/AmmoPickup.cs
+++ Skylduverk-leikur/Assets/scripts/AmmoPickup.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class AmmoPickup : MonoBehaviour
 {
+    public AudioSource ammoSound;
 
-
-    // Þegar leikmaðurinn rekstá skotfærageymsluna þá hækkar magnið á skotfærum um 10 stig
+    // Þegar leikmaðurinn rekst á skotfærageymsluna þá hækkar magnið á skotfærum um 10 stig og ammo hljóð er spilað
     void OnTriggerEnter (Collider col) 
     {
+        ammoSound.Play();
         Skotfæri.CurrentAmmo += 10;
         this.gameObject.SetActive(false);
     }
