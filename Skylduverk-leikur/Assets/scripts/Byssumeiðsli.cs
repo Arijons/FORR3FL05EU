b89e61a8b8afecbf0ba27fec524d6625cd7c1c1a
Ari Jónsson <arijons20@gmail.com>
2019-10-16 20:27:22 +0000

    laga óvinameiðsli

--- Skylduverk-leikur/Assets/scripts/Byssumeiðsli.cs
+++ Skylduverk-leikur/Assets/scripts/Byssumeiðsli.cs
@@ -25,7 +25,7 @@ public class Byssumeiðsli : MonoBehaviour
 
             {
                 if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Skot))
-                {   // athuga hvað óvinur margar metra frá byssunni með RyscastHit skipun 
+                {   // athuga hvað óvinur margar metra frá byssunni með RaycastHit skipun 
                     FjarlægðÓvinar = Skot.distance;
                     if (FjarlægðÓvinar < ByssaDrifur)
                     {   // ef byssan er nóg nálægt þá lækkar heilsan hjá óvinum um 5
