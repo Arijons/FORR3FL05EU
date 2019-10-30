bc28fbb01794bc250f076efe1f8eab87be40fe9b
Ari Jónsson <arijons20@gmail.com>
2019-10-30 21:34:48 +0000

    Takaupp 9mm byssu

--- Skylduverk-leikur/Assets/scripts/TakaUpp9mm.cs
+++ Skylduverk-leikur/Assets/scripts/TakaUpp9mm.cs
@@ -5,27 +5,50 @@ using UnityEngine;
 public class TakaUpp9mm : MonoBehaviour
 {
     public float TheDistance = PlayerCasting.DistanceFromTarget;
-    public GameObject Textdisplay;
+    public GameObject TextDisplay;
     public GameObject FakeGun;
     public GameObject RealGun;
+    public GameObject AmmoDisplay;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         TheDistance = PlayerCasting.DistanceFromTarget;
         if (Input.GetButtonDown("Action"))
         {
             if (TheDistance <= 2)
             {
-                TakaUpp9mm();
-             
+                TakaUpp9mmByssu();
             }
         }
+    }
+
+    private void OnMousOver()
+    {
+        if (TheDistance <= 2)
+        {
+            TextDisplay.GetComponent<GUIText>().text = "Taka 9mm byssu";
+        }
+            
+    }
+
+    private void OnMousExit()
+    {
+        TextDisplay.GetComponent<GUIText>().text = "";
+    }
+
+    private void TakaUpp9mmByssu ()
+    {
+        transform.position = new Vector3(0,-1000,0);
+        FakeGun.SetActive(false);
+        RealGun.SetActive(true);
+        AmmoDisplay.SetActive(true);
     }
 }
 
