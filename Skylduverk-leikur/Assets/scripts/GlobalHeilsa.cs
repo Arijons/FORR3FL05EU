174b1e0875e2b588d91097b5605242181ebcfbbb
Ari Jónsson <arijons20@gmail.com>
2019-12-01 22:02:22 +0000

    Game over og heilsa

---
+++ Skylduverk-leikur/Assets/scripts/GlobalHeilsa.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GlobalHeilsa : MonoBehaviour
+
+{
+
+    public static int PlayerHealth = 10;
+    public int InternalHealth;
+    public GameObject HeilsuMiði;
+
+    void Update() // ef helisan er == 0 er farið beint í game over senuna
+
+    {
+        InternalHealth = PlayerHealth;
+        HeilsuMiði.GetComponent<Text>().text = "Heilsa: " + PlayerHealth;
+        if (PlayerHealth == 0)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
+
+
+
