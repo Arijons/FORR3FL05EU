fb542da1aa95c0b3c2856a6c40e1f1b3676f5a6b
Ari Jónsson <arijons20@gmail.com>
2019-12-01 22:56:29 +0000

    ovinur eltir

--- Skylduverk-leikur/Assets/scripts/PlayerCasting.cs
+++ Skylduverk-leikur/Assets/scripts/PlayerCasting.cs
@@ -21,3 +21,5 @@ public class PlayerCasting : MonoBehaviour
 
     }
 }
+
+
---
+++ Skylduverk-leikur/Assets/scripts/Óvinahreyfing.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class Óvinahreyfing : MonoBehaviour
+{
+
+    public GameObject ThePlayer;
+    public float TargetDistance;
+    public float AllowedRange = 10;
+    public GameObject TheEnemy;
+    public float EnemySpeed;
+    public int MoveTrigger;
+    //public RaycastHit Shot;
+
+    void Update()
+    {
+        // transform.LookAt(ThePlayer.transform);
+        if (MoveTrigger == 1)
+        {
+            EnemySpeed = 0.02f;
+            transform.position = Vector3.MoveTowards(transform.position, ThePlayer.transform.position, EnemySpeed);
+        }
+    }
+}
+       
